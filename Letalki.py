b7e32cd032a3d6240a8cf652a402f14a640459f9
KostyaShadrin <71694759+KostyaShadrin@users.noreply.github.com>
2020-10-13 11:13:07 +0300

    Тыкай по шарикам
    Обновлен график, добавлена похвала)

--- Letalki.py
+++ Letalki.py
@@ -4,24 +4,38 @@ from pygame.draw import *
 from random import randint
 import os
 import matplotlib
+import matplotlib.pyplot
 
 matplotlib.use("Agg")
 import matplotlib.backends.backend_agg as agg
 
 import pylab
 
+# Цвета
+if 0 == 0:
+    RED = (255, 0, 0)
+    BLUE = (0, 0, 255)
+    YELLOW = (255, 255, 0)
+    GREEN = (0, 255, 0)
+    MAGENTA = (255, 0, 255)
+    CYAN = (0, 255, 255)
+    BLACK = (0, 0, 0)
+    WHITE = (255, 255, 255)
+    COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
+
 fig = pylab.figure(figsize=[6, 4],  # Inches
                    dpi=200,  # 100 dots per inch, so the resulting buffer is 400x400 pixels
                    )
 grafic = fig.gca()
-B=[100]*20
-A = [0]*20
+B = [100] * 20
+A = [0] * 20
+matplotlib.pyplot.xlabel(r'Время')
+matplotlib.pyplot.ylabel(r"Успешность")
 grafic.plot(A)
 canvas = agg.FigureCanvasAgg(fig)
 canvas.draw()
 renderer = canvas.get_renderer()
 raw_data = renderer.tostring_rgb()
-
 os.environ['SDL_VIDEO_CENTERED'] = '1'
 
 pygame.init()
@@ -35,26 +49,19 @@ screen.blit(surf, (0, 0))
 clock = pygame.time.Clock()
 X1, Y1, X2, Y2 = 0, 95, 1200, 800
 
+def KRASAWWWA():
+    inscription_font = pygame.font.SysFont('Arial Black', 60)
+    inscription = inscription_font.render("KRASAWWWA, 100% POPADANIE",5,(randint(1,254),randint(1,254),randint(1,254)))
+    screen.blit(inscription, (100, 0))
 
-def chiska():
+def chiska(uspshnost):
     screen.blit(surf, (0, 0))
     polygon(screen, GREEN, [(X1, Y1), (X1, Y2), (X2, Y2), (X2, Y1)], 5)
     score()
     for n in range(N):
         circle(screen, circs[n][3], (circs[n][0], circs[n][1]), circs[n][2])
-
-
-# Цвета
-if 0 == 0:
-    RED = (255, 0, 0)
-    BLUE = (0, 0, 255)
-    YELLOW = (255, 255, 0)
-    GREEN = (0, 255, 0)
-    MAGENTA = (255, 0, 255)
-    CYAN = (0, 255, 255)
-    BLACK = (0, 0, 0)
-    WHITE = (255, 255, 255)
-    COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
+    if (uspeshnost == 100) and (popal > 0):
+        KRASAWWWA()
 
 
 # Новый шарик
@@ -94,10 +101,13 @@ def check(n, x, y):
     else:
         return False
 
+
 def sdvig(c):
     for i in range(19):
-        A[i]=A[i+1]
-    A[19]=c
+        A[i] = A[i + 1]
+    A[19] = c
+
+
 N = 8
 circs = [0] * 15
 for i in range(N):
@@ -108,21 +118,22 @@ for i in range(N):
 pygame.display.update()
 finished = False
 promazal = 0
-promazal_prev=0
+promazal_prev = 0
 popal = 1
-popal_prev=0
+popal_prev = 0
 s = 0
 time0 = 0
+uspeshnost =0
 score()
 pygame.display.update()
 time0 = pygame.time.get_ticks()
 start_time = pygame.time.get_ticks()
 while not finished:
     clock.tick(FPS)
-    chiska()
+    chiska(uspeshnost)
 
     for n in range(N):
-        if (circs[n][0] - circs[n][2] < X1) or (circs[n][0] + circs[n][2] > X2):#Проверка стены
+        if (circs[n][0] - circs[n][2] < X1) or (circs[n][0] + circs[n][2] > X2):  # Проверка стены
             circs[n][4] *= -1
             circs[n][0] += circs[n][4]
         elif (circs[n][1] - circs[n][2] < Y1) or (circs[n][1] + circs[n][2] > Y2):
@@ -131,30 +142,38 @@ while not finished:
         elif circs[n][2] > 90:  # Такой порядок для того, чтобы не улетали за границ, Уничтожение
             promazal += 1
             new_ball(n)
-        elif (pygame.time.get_ticks() - circs[n][6]) > 60:#Увеличение
+        elif (pygame.time.get_ticks() - circs[n][6]) > 60:  # Увеличение
             circs[n][2] += 1
             circs[n][6] = pygame.time.get_ticks()
-        circs[n][0] += circs[n][4]#Передижение
+        circs[n][0] += circs[n][4]  # Передижение
         circs[n][1] += circs[n][5]
         circle(screen, circs[n][3], (circs[n][0], circs[n][1]), circs[n][2])
     if pygame.time.get_ticks() - time0 > 2000:
         time0 = pygame.time.get_ticks()
-        sdvig(100*(popal-popal_prev)/(popal+promazal-popal_prev-promazal_prev))
-        promazal_prev=promazal
-        popal_prev=popal
-        B=[(popal*100)/(popal+promazal)]*20
+        if (popal + promazal - popal_prev - promazal_prev)==0:
+            uspeshnost=0
+        else:
+            uspeshnost=100*(popal - popal_prev) / (popal + promazal - popal_prev - promazal_prev)
+        sdvig(uspeshnost)
+        promazal_prev = promazal
+        popal_prev = popal
+        B = [(popal * 100) / (popal + promazal)] * 20
 
         fig = pylab.figure(figsize=[6, 4],  # Inches
                            dpi=200,  # 100 dots per inch, so the resulting buffer is 400x400 pixels
                            )
+        matplotlib.pyplot.xlabel(r'Время')
+        matplotlib.pyplot.ylabel(r"Успешность")
         grafic = fig.gca()
-        grafic.plot(B)
-        grafic.plot(A)
+        grafic.plot(B, color='g')
+        grafic.plot(A, color='b')
         canvas = agg.FigureCanvasAgg(fig)
         canvas.draw()
         renderer = canvas.get_renderer()
         raw_data = renderer.tostring_rgb()
         surf = pygame.image.fromstring(raw_data, size, "RGB")
+
+
     pygame.display.update()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -162,7 +181,7 @@ while not finished:
         elif event.type == pygame.MOUSEBUTTONDOWN:
             for i in range(N):
                 if check(i, event.pos[0], event.pos[1]):  # Проверка на попадание
-                    chiska()
+                    chiska(uspeshnost)
                     popal += 1
                     s = 1
                     new_ball(i)
