bebbade3db5ea91c54ef8d34c472ed2793e3de6f
kuznets26 <kuznetsov.gs@phystech.edu>
2020-11-03 00:54:47 +0300

    Brand new features

--- pushka.py
+++ pushka.py
@@ -1,9 +1,9 @@
 from random import randrange as rnd, choice
+from random import randint
+import random
 import tkinter as tk
 import math
 import time
-
-# print (dir(math))
 
 root = tk.Tk()
 fr = tk.Frame(root)
@@ -14,17 +14,20 @@ canv.pack(fill=tk.BOTH, expand=1)
 score = 0
 
 
+# Хранит информацию о шарике
 class ball():
-    def __init__(self, x=40, y=450):
+    def __init__(self, x=40, y=450, m=6600):
         """ Конструктор класса ball
 
         Args:
         x - начальное положение мяча по горизонтали
         y - начальное положение мяча по вертикали
         """
+        self.rho = 14
         self.x = x
         self.y = y
-        self.r = 10
+        self.r = math.sqrt(m / (self.rho * math.pi))
+        self.m = m
         self.vx = 0
         self.vy = 0
         self.t_live = 0
@@ -37,7 +40,18 @@ class ball():
             fill=self.color
         )
         self.live = 30
+        self.i = ball.add_ball(self)
 
+    def add_ball(b):
+        global balls
+        balls += [b]
+        return len(balls) - 1
+
+    def remove_ball(i):
+        global balls
+        balls[i] = 0
+
+    # Задает координаты шарика
     def set_coords(self):
         canv.coords(
             self.id,
@@ -54,7 +68,6 @@ class ball():
         self.x и self.y с учетом скоростей self.vx и self.vy, силы гравитации, действующей на мяч,
         и стен по краям окна (размер окна 800х600).
         """
-        # FIXME
         if self.x - self.r < 0:
             self.vx *= -1
         if self.x + self.r > 800:
@@ -80,6 +93,7 @@ class ball():
             return True
         return False
 
+    # Рисует мячик
     def draw(self):
         canv.coords(self,
                     self.x - self.r,
@@ -88,17 +102,58 @@ class ball():
                     self.y + self.r
                     )
 
+    # Удаляет мячик как объект
     def deletes(self):
         canv.delete(self.id)
+        ball.remove_ball(self.i)
 
 
+class cool_ball(ball):
+    def __init__(self, x=40, y=450, m=6600):
+        self.explode_time = random.random()
+        self.creation_time = time.time()
+        ball.__init__(self, x, y, m)
+
+    def divide(n, negative_allowed=1):
+        parts = [0] * n
+        s = 0
+        for i in range(n):
+            k = 1 - 2 * randint(0, negative_allowed)
+            parts[i] = k * random.random()
+            s += parts[i]
+        if abs(s) < 0.001:
+            parts[0] += 1
+            s += 1
+        return parts, s
+
+    def explode(self):
+        global balls
+        self.deletes()
+        n = rnd(1, 5, 1)
+        parts, s = cool_ball.divide(n, 0)
+        print(parts, s)
+        p_x, s_p_x = cool_ball.divide(n, 1)
+        p_y, s_p_y = cool_ball.divide(n, 1)
+        for i in range(n):
+            m_small = parts[i] * self.m / s
+            b = ball(self.x, self.y, m_small)
+            b.vx = p_x[i] * self.vx * self.m / (s_p_x * m_small)
+            b.vy = p_y[i] * self.vy * self.m / (s_p_y * m_small)
+
+    def explosion_test(self):
+        if time.time() - self.creation_time > self.explode_time:
+            self.explode()
+
+
+# Хранит информацию о пушке
 class gun():
     def __init__(self):
         self.f2_power = 10
         self.f2_on = 0
         self.an = 1
-        self.id = canv.create_line(20, 450, 50, 420, width=7)  # FIXME: don't know how to set it...
+        self.id = canv.create_line(20, 450, 50, 420, width=7)
 
+    # Определяет начальную силу
     def fire2_start(self, event):
         self.f2_on = 1
 
@@ -110,12 +165,14 @@ class gun():
         """
         global balls, bullet
         bullet += 1
-        new_ball = ball()
-        new_ball.r += 5
+        l = randint(1, 4)
+        if l > 2:
+            new_ball = cool_ball()
+        else:
+            new_ball = ball()
         self.an = math.atan((event.y - new_ball.y) / (event.x - new_ball.x))
         new_ball.vx = self.f2_power * math.cos(self.an)
         new_ball.vy = - self.f2_power * math.sin(self.an)
-        balls += [new_ball]
         self.f2_on = 0
         self.f2_power = 10
 
@@ -132,6 +189,7 @@ class gun():
                     450 + max(self.f2_power, 20) * math.sin(self.an)
                     )
 
+    # Увеличение силы
     def power_up(self):
         if self.f2_on:
             if self.f2_power < 100:
@@ -141,6 +199,7 @@ class gun():
             canv.itemconfig(self.id, fill='black')
 
 
+# Создает цель для огня
 class target():
     def __init__(self):
         self.points = 0
@@ -176,6 +235,7 @@ class target():
         global score
         score += int((500 / bullet) / self.r)
 
+    # Двигает цель
     def move(self):
         if self.x - self.r < 0:
             self.vx = -self.vx
@@ -189,6 +249,7 @@ class target():
         self.x += self.vx
         self.y -= self.vy
 
+    # Рисует цель
     def draw(self):
         x = self.x
         y = self.y
@@ -197,6 +258,7 @@ class target():
         canv.coords(self.id, x - r, y - r, x + r, y + r)
         canv.itemconfig(self.id, fill=color)
 
+    # Отводит цель на дальнее расстояние
     def outbound(self):
         x = self.x = -1000
         self.vx = 0
@@ -206,89 +268,7 @@ class target():
         color = self.color = 'red'
         canv.coords(self.id, x - r, y - r, x + r, y + r)
         canv.itemconfig(self.id, fill=color)
-"""
-class Shape_target():
-    def __init__(self):
-        self.x = rnd(100, 700)  # x
-        self.y = rnd(100, 500)  # y
-        self.r = rnd(30, 50)  # radius
-        self.colors = ''  # colors
-        self.vx = rnd(-500, 500)  # vx - speed xlabel
-        self.vy = rnd(-500, 500)  # vy - speed ylabel
-        self.angles = rnd(3, 9)  # quantity of angles
-        self.rotation_angle = rnd(0, round(2 * m.pi))  # rotation angle
-        self.omega = rnd(0, round(5 * m.pi))  # OMEGA motherfucker do you speak it???
-        
-    def create_shape(self):
-        self.x = rnd(100, 700)  # x
-        self.y = rnd(100, 500)  # y
-        self.r = rnd(30, 50)  # radius
-        self.colors = ''  # colors
-        self.vx = rnd(-500, 500)  # vx - speed xlabel
-        self.vy = rnd(-500, 500)  # vy - speed ylabel
-        self.angles = rnd(3, 9)  # quantity of angles
-        self.rotation_angle = rnd(0, round(2 * m.pi))  # rotation angle
-        self.omega = rnd(0, round(5 * m.pi))  # OMEGA motherfucker do you speak it????
 
-    # draws this shape
-    def draw_shape(self):
-        # defines vertices of the polygon
-        shape_vertices = [0] * self.angles
-        # counting an angle between two vertices
-        alpha = 2 * m.pi / self.angles
-        # creating a list of vertices
-        for i in range(self.angles):
-            shape_vertices[i] = (
-            round(self.x + self.r * math.cos(self.rotation_angle + alpha * i)),
-            round(s + Shape_Parameters[self][2] * math.sin(self.rotation_angle + alpha * i)))
-        polygon(screen, Shape_Parameters[self][3], shape_vertices)
-
-    # makes the world fly (moving and rotating the shape)
-    def move_shape(n):
-        global Shape_Parameters
-        Shape_Parameters[n][0] += Shape_Parameters[n][4] / FPS
-        Shape_Parameters[n][1] += Shape_Parameters[n][5] / FPS
-        # implementing rotation
-        Shape_Parameters[n][7] += Shape_Parameters[n][8] / FPS
-        if Shape_Parameters[n][0] - Shape_Parameters[n][2] < 0:
-            Shape_Parameters[n][4] = - Shape_Parameters[n][4]
-        if Shape_Parameters[n][1] - Shape_Parameters[n][2] < 0:
-            Shape_Parameters[n][5] = - Shape_Parameters[n][5]
-        if Shape_Parameters[n][0] + Shape_Parameters[n][2] > width_screen:
-            Shape_Parameters[n][4] = - Shape_Parameters[n][4]
-        if Shape_Parameters[n][1] + Shape_Parameters[n][2] > height_screen:
-            Shape_Parameters[n][5] = - Shape_Parameters[n][5]
-
-    # checking if we hit the shape
-    def shape_handle_event_hit(event, i):
-        global hit
-        global Shape_Parameters
-        # counting an angle between two vertices
-        alpha = 2 * m.pi / Shape_Parameters[i][6]
-        f = True
-        for j in range(Shape_Parameters[i][6]):
-            # counting vector between 2 neighbouring vertices
-            a_x = Shape_Parameters[i][0] + Shape_Parameters[i][2] * m.cos(Shape_Parameters[i][7] + alpha * j)
-            a_y = Shape_Parameters[i][1] + Shape_Parameters[i][2] * m.sin(Shape_Parameters[i][7] + alpha * j)
-            b_x = Shape_Parameters[i][0] + Shape_Parameters[i][2] * m.cos(Shape_Parameters[i][7] + alpha * (j + 1))
-            b_y = Shape_Parameters[i][1] + Shape_Parameters[i][2] * m.sin(Shape_Parameters[i][7] + alpha * (j + 1))
-            # counting vector between the first neighbouring vertex and click position
-            ev_point_x = event.pos[0] - a_x
-            ev_point_y = event.pos[1] - a_y
-            v_x = b_x - a_x
-            v_y = b_y - a_y
-            # counting the determinant(oriented area)
-            det = - ev_point_x * v_y + ev_point_y * v_x
-            if det < 0:
-                f = False
-                break
-        if f:
-            hit += 5
-            create_inscription()
-            create_shape(i)
-            return True
-        return False
-"""
 
 t1 = target()
 t2 = target()
@@ -301,8 +281,9 @@ targets = [t1, t2]
 k = 0
 
 
+# Основная функция игры
 def new_game(event=''):
-    global gun, t1, screen1, balls, bullet, gravitation, targets, t2, k
+    global gun, t1, screen1, balls, bullet, gravitation, targets, t2, k, screen2
     canv.itemconfig(screen1, text='')
     bullet = 0
     balls = []
@@ -315,45 +296,50 @@ def new_game(event=''):
     for t in targets:
         t.new_target()
         t.live = 1
-    print(k)
     while k > 0:
-        for b in balls:
-            b.move()
-            canv.itemconfig(screen2, text=score)
-            for t in targets:
-                if b.hittest(t) == True:
-                    t.live = 0
-                    b.deletes()
-                    t.outbound()
-                    t.hit()
-                    k -= 1
-            if k <= 0:
-                print('Balls')
-                for b in balls:
-                    b.deletes()
-                canv.itemconfig(screen1, text='Вы уничтожили цель за ' + str(bullet) + ' выстрелов')
-                canv.update()
-                time.sleep(1.5)
-                canv.itemconfig(screen1, text='Новая игра через 3...')
-                canv.update()
-                time.sleep(1)
-                canv.itemconfig(screen1, text='Новая игра через 2...')
-                canv.update()
-                time.sleep(1)
-                canv.itemconfig(screen1, text='Новая игра через 1...')
-                canv.update()
-                time.sleep(1)
-                canv.itemconfig(screen1, text='Поехали!')
-                canv.update()
-                time.sleep(0.5)
-                canv.bind('<Button-1>', '')
-                canv.bind('<ButtonRelease-1', '')
-                root.after(1000, new_game)
+        for i in range(len(balls)):
+            b = balls[i]
+            if b != 0:
+                if type(b) == cool_ball:
+                    b.explosion_test()
+                b.move()
+                canv.itemconfig(screen2, text=score)
+                for t in targets:
+                    if b.hittest(t):
+                        t.live = 0
+                        b.deletes()
+                        t.outbound()
+                        t.hit()
+                        k -= 1
+                if k <= 0:
+                    for b in balls:
+                        if b != 0:
+                            b.deletes()
+                    canv.itemconfig(screen1, text='Вы уничтожили цель за ' + str(bullet) + ' выстрелов')
+                    canv.update()
+                    time.sleep(1.5)
+                    canv.itemconfig(screen1, text='Новая игра через 3...')
+                    canv.update()
+                    time.sleep(1)
+                    canv.itemconfig(screen1, text='Новая игра через 2...')
+                    canv.update()
+                    time.sleep(1)
+                    canv.itemconfig(screen1, text='Новая игра через 1...')
+                    canv.update()
+                    time.sleep(1)
+                    canv.itemconfig(screen1, text='Поехали!')
+                    canv.update()
+                    time.sleep(0.5)
+                    canv.bind('<Button-1>', '')
+                    canv.bind('<ButtonRelease-1', '')
+                    k = 0
+                    root.after(1000, new_game)
         for t in targets:
             t.move()
             t.draw()
         for b in balls:
-            b.draw()
+            if b != 0:
+                b.draw()
         canv.update()
         time.sleep(z)
         g1.targetting()
